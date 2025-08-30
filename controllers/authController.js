424f20e02ceca9230397545f5d549de105dd99e0
Neil <Neil10241126@gmail.com>
2025-08-30 16:47:55 +0800

    feat: add login api for auth routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,6 +69,76 @@ const signUp = async (req, res) => {
   }
 }
 
+
+/**
+ * @openapi
+ * /api/v1/auth/login:
+ *   post:
+ *     tags: [Auth]
+ *     summary: Login a new user
+ *     description: Login a new user
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: "`SUCCESS`"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: { type: number, example: 200 }
+ *                 code: { type: string, example: 'SUCCESS' }
+ *                 message: { type: string }
+ *       '401':
+ *         description: |
+ *           - `EMAIL_NOT_FOUND`: Email not found
+ *           - `PASSWORD_NOT_CORRECT`: Password not correct
+ */
+const login = async (req, res) => {
+  const { email, password } = req.body
+
+  const [rows] = await query(`
+    SELECT user_id, email, password
+    FROM users
+    WHERE email = ?
+  `, [email])
+
+  if (rows.length === 0) {
+    return res.status(401).json({
+      status: 401,
+      code: 'EMAIL_NOT_FOUND',
+      message: 'Email not found',
+    })
+  }
+
+  const user = rows[0]
+  const ok = await argon2.verify(user.password, password)
+
+  if (!ok) {
+    return res.status(401).json({
+      status: 401,
+      code: 'PASSWORD_NOT_CORRECT',
+      message: 'Password not correct',
+    })
+  }
+
+  res.status(200).json({
+    status: 200,
+    code: 'SUCCESS',
+    message: 'Sign in success',
+  })
+}
+
 export {
-  signUp
+  signUp,
+  login
 }
--- routers/auth.js
+++ routers/auth.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signUp } from '../controllers/authController.js';
+import { signUp, login } from '../controllers/authController.js';
 import { signUpSchema } from '../schemas/auth.js';
 import { validate } from '../middleware/validate.js';
 
 const router = express.Router();
 
 router.post('/sign-up', validate(signUpSchema), signUp)
+router.post('/login', login)
 
 export default router;
