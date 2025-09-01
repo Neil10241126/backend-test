f228ee2a46d77afde69f1cfdefb89e6a70936ce2
Neil <Neil10241126@gmail.com>
2025-09-01 13:58:09 +0800

    feat: add port with database

--- db/pool.js
+++ db/pool.js
@@ -8,8 +8,13 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 }).promise();
 
 const query = async (sql, params) => pool.query(sql, params)
+
+query('SELECT 1')
+  .then(() => console.log('DB OK:', process.env.MYSQL_HOST))
+  .catch(e => console.error('DB FAIL:', e.message));
 
 export default query
